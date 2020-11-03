cdffce7e53641d3bc0a0fc857c7bfe7f613b9912
Spike <jchoi166@gmail.com>
2020-11-03 06:34:13 -0800

    finished card tags. Working on search view and tag--clear

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,28 +1,28 @@
 import { data } from "../../data"
 
-// export const test = data[0]
 
-export const test = () => {
-    // console.log(data[0].logo)
-
-    let imagePath = "." + data[0].logo
-    console.log(imagePath)
+const generateTags = (tagName)=> {
+    for (let i of tagName) {
+        return `<span class="button button--tag">${i}</span>`
+    }
 }
+
+const test = () => console.log('fasfsadworking')
+
 export const renderCard = card => {
 
-    // // let imagePath = 
-    // let imagePath = "." + card.logo
+    let img = card.logo
     
     const markup = `
     <div class="card">
       <div class="card__logo">
-        <img src=${card.logo} alt="lol">
+        <img src=".${img}" alt="lol">
       </div>
       <div class="card__profile">
         <div class="card__heading">
           <span class="card__heading-caption card__heading-caption--title">${card.company}</span>
-          <span class="card__heading-caption card__heading-caption--new">NEW!</span>
-          <span class="card__heading-caption card__heading-caption--featured">Featured</span>
+          ${card.new ? '<span class="card__heading-caption card__heading-caption--new">NEW!</span>' : ''}
+          ${card.featured ? '<span class="card__heading-caption card__heading-caption--featured">Featured</span>' : ''}
         </div>
         <div class="card__title">
           ${card.position}
@@ -37,7 +37,9 @@ export const renderCard = card => {
         <hr class="card__separator">
       </div>
       <div class="card__tag-container">
-        <span class="button--tag">Frontend</span>
+        ${card.tools.length > 0 ? generateTags(card.tools) : ''}
+        
+        ${card.languages.length > 0 ? generateTags(card.languages) : ''}
       </div>
     </div>
     
